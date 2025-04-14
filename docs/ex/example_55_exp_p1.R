4f6ce3b24e8cfc79b6b92077c27dcaab8305e198
stephenjewson <93211466+stephenjewson@users.noreply.github.com>
2025-04-14 17:57:44 +0100

    Add files via upload

---
+++ docs/ex/example_55_exp_p1.R
@@ -0,0 +1,14 @@
+#
+# example 1
+x=fitdistcp::d55exp_p1_example_data_v1_x
+tt=fitdistcp::d55exp_p1_example_data_v1_t
+p=c(1:9)/10
+n0=10
+q=qexp_p1_cp(x,tt,n0=n0,p=p,rust=TRUE,nrust=1000)
+xmin=min(q$ml_quantiles,q$cp_quantiles);
+xmax=max(q$ml_quantiles,q$cp_quantiles);
+plot(q$ml_quantiles,p,xlab="quantile estimates",xlim=c(xmin,xmax),
+	sub="(from qexp_p1_cp)",
+	main="Exponential w/ p1: quantile estimates");
+points(q$cp_quantiles,p,col="red",lwd=2)
+points(q$ru_quantiles,p,col="blue")
---
+++ docs/ex/example_61_lnorm_p1.R
@@ -0,0 +1,14 @@
+#
+# example 1
+x=fitdistcp::d61lnorm_p1_example_data_v1_x
+tt=fitdistcp::d61lnorm_p1_example_data_v1_t
+p=c(1:9)/10
+n0=10
+q=qlnorm_p1_cp(x,tt,n0=n0,p=p,rust=TRUE,nrust=1000)
+xmin=min(q$ml_quantiles,q$cp_quantiles);
+xmax=max(q$ml_quantiles,q$cp_quantiles);
+plot(q$ml_quantiles,p,xlab="quantile estimates",xlim=c(xmin,xmax),
+	sub="(from qlnorm_p1_cp)",
+	main="Log-Normal w/ p1: quantile estimates");
+points(q$cp_quantiles,p,col="red",lwd=2)
+points(q$ru_quantiles,p,col="blue")
